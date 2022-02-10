30d98c93573641f57c4a07a1fc173621373fff9f
Taylor Britt <99290569+tbritt6@users.noreply.github.com>
2022-02-10 16:00:22 -0500

    Update ShinyApp.R
    Super simple shiny app

--- ShinyApp.R
+++ ShinyApp.R
@@ -1,11 +1,15 @@
 library(shiny)
 
 ui <- fluidPage(
+  textInput("text", label = h3("Please Enter your name"), value = "Last name, First name"),
+  
+  hr(),
+  fluidRow(column(3, verbatimTextOutput("value")))
   
 )
 
 server <- function(input, output, session) {
-  
+  output$value <- renderPrint({ input$text })
 }
 
 shinyApp(ui, server)
