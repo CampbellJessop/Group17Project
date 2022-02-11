3f4c337d022246a2ed5e58257a0ec7291b02c2b6
Cora-Hansen <znk562@vols.utk.edu>
2022-02-11 14:52:05 -0500

    Update ShinyApp.R
    Created a select a purpose option

--- ShinyApp.R
+++ ShinyApp.R
@@ -3,11 +3,18 @@ library(fpp3)
 data("tourism")
 
 ui <- fluidPage(
-  plotOutput("plot")
+  plotOutput("plot"),
+  selectInput("Purpose", label = h3("Select Purpose"), 
+              choices = list("Business", "Holiday", "Visiting", "Other"), 
+              selected = 1),
+  hr(),
+  fluidRow(column(3, verbatimTextOutput("value")))
 )
 
 server <- function(input, output, session) {
-  output$plot <- renderPlot({autoplot(filter(tourism, Region == "Adelaide"))})
+  output$plot <- renderPlot({autoplot(filter(tourism, Region == "Adelaide", Purpose == input$Purpose))})
+  output$value <- renderPrint({ input$Purpose })
 }
 
 shinyApp(ui, server)
+
